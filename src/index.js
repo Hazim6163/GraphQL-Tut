7eabbc77ad05b1535f878856a97045c489c8ae6c
Mustafa <hazim6163@gmail.com>
2020-04-28 18:23:22 +0200

    code reformmat

--- src/index.js
+++ src/index.js
@@ -1,22 +1,22 @@
-const {GraphQLServer} = require('graphql-yoga');
+const { GraphQLServer } = require('graphql-yoga');
 
 const links = [
-    {id:'link-0', des: 'Google', url: 'www.google.com'},
-    {id:'link-1', des: 'facebook', url: 'www.facebook.com'}
+    { id: 'link-0', des: 'Google', url: 'www.google.com' },
+    { id: 'link-1', des: 'facebook', url: 'www.facebook.com' }
 ]
 
 let id = links.length;
 
 const resolvers = {
     Query: {
-      info: () => `Hello World`,
-      feed: () => links,
-      link: (parent, args) => {
-          const link = links.find(e => {
-              return e.id === args.id
-          })
-          return link;
-      }
+        info: () => `Hello World`,
+        feed: () => links,
+        link: (parent, args) => {
+            const link = links.find(e => {
+                return e.id === args.id
+            })
+            return link;
+        }
     },
     Mutation: {
         post: (parent, args) => {
@@ -30,13 +30,13 @@ const resolvers = {
         },
         updateLink: (parent, args) => {
             const link = links.find(e => e.id === args.id);
-            link.des = args.des; 
+            link.des = args.des;
             link.url = args.url;
             return link;
         }
     }
 }
-  
+
 
 const server = new GraphQLServer({
     typeDefs: './src/schema.graphql',
