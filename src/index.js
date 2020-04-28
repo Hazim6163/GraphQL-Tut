fc644bd84c3d3ae50cab13287d872fda01cb6853
Mustafa <hazim6163@gmail.com>
2020-04-28 18:07:31 +0200

    simle mutation

--- src/index.js
+++ src/index.js
@@ -1,19 +1,32 @@
 const {GraphQLServer} = require('graphql-yoga');
 
 const links = [
-    {id: 1, des: 'some random link', url: 'www.google.com'}
+    {id:'link-0', des: 'some random link', url: 'www.google.com'}
 ]
+
+let id = links.length;
 
 const resolvers = {
     Query: {
       info: () => `Hello World`,
       feed: () => links
+    },
+    Mutation: {
+        post: (parent, args) => {
+            const link = {
+                id: `link-${id++}`,
+                des: args.des,
+                url: args.url
+            }
+            links.push(link);
+            return link;
+        }
     }
 }
   
 
 const server = new GraphQLServer({
-    typeDefs: './schema.graphql',
+    typeDefs: './src/schema.graphql',
     resolvers
 })
 
